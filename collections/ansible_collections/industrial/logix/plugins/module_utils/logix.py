4ccc53f11a975ec7d220f7cab77e726f71d5db8e
matoval <mat24c@gmail.com>
2022-07-28 14:42:16 -0700

    Update ensure_tags to handle multiple tags

--- collections/ansible_collections/industrial/logix/plugins/module_utils/logix.py
+++ collections/ansible_collections/industrial/logix/plugins/module_utils/logix.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import atexit
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection, ConnectionError
 try:
@@ -19,11 +20,25 @@ class LogixUtil(object):
         self.connection = Connection(self.module._socket_path)
         self.logix_address = self.connection.get_option('host')
 
-        # Sanity test
-        with LogixDriver(self.logix_address) as plc:
-            if not plc.connected:
-                self.module.fail_json(
-                    "Unable to connect to ControlLogix device: %s" % self.logix_address
-                )
-            self.plc = plc
+        self.plc = LogixDriver(self.logix_address)
+        self.plc.open()
 
+        
+        if not self.plc.connected:
+            self.module.fail_json(
+                "Unable to connect to ControlLogix device: %s" % self.logix_address
+            )
+
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        self.plc.close()
+
+    def typecast_plc_value(self, plc_data_type, tag_value):
+        if plc_data_type == 'BOOL':
+            tag_value = tag_value.lower() in ['true', '1', 't', 'y', 'yes', 'false', '0', 'f', 'n', 'no']
+        elif plc_data_type == 'REAL' or plc_data_type == 'FLOAT':
+            tag_value = float(tag_value)
+        elif plc_data_type == 'DINT' or plc_data_type == 'DINT':
+            tag_value = int(tag_value)
+        return tag_value
