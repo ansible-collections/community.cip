3e289e0b218d1740ad985792f5f62a28d0531c88
Adam Miller <admiller@redhat.com>
2022-10-04 13:37:44 -0500

    initial sanity checking github actions (#2)
    * initial sanity checking github actions
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * fixing sanity tests
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * more sanity test stuff
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * even more sanity checks passing ... down to one
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * local pycomm3 import error for sanity tests still persists
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * iterating on fixing sanity tests
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * sanity test fixing, works with python 3.10 and ansible-core 2.12.x
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * ignore
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * ignores
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * more ignores
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * more ignores
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * fix even more sanity failures
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * missed one
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * don't run on python2
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * run tests please
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * run tests please ... the remix
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    * 2.11 EOLs in a few weeks, drop it from CI
    
    Signed-off-by: Adam Miller <admiller@redhat.com>
    
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- plugins/modules/ensure_firmware_version.py
+++ plugins/modules/ensure_firmware_version.py
@@ -16,7 +16,7 @@ ANSIBLE_METADATA = {
 
 DOCUMENTATION = """
 ---
-module: ensure_firmware_revision
+module: ensure_firmware_version
 short_description: Ensure firmware is a specific revision or fail
 description:
     - Ensure firmware is a specific revision or fail
@@ -26,8 +26,8 @@ options:
   revision:
     description:
       - firmware revision to validate in the form XX.YYY or XX
-    required: false
-    type: int
+    required: true
+    type: str
 """
 
 EXAMPLES = """
@@ -50,7 +50,6 @@ from ansible_collections.industrial.logix.plugins.module_utils.logix import Logi
 
 def main():
 
-     
     argspec = dict(
         revision=dict(required=True, type="str"),
     )
@@ -61,116 +60,143 @@ def main():
 
     logix_util = LogixUtil(module)
 
-    plc_revision = logix_util.plc.get_plc_info()['revision']
+    plc_revision = logix_util.plc.get_plc_info()["revision"]
 
     # The best I can tell, all ControlLogix controllers revisions are composed
     # of a two digit number signifying the "Major" revision, followed by a dot,
     # followed by a three digit number signifying the "Minor" revision: XX.YYY.
     # Larger major numbers correspond to newer major revisions (typically one
     # major release per year but there are exceptions). Minor releases are less
-    # reliable in that regard. For example minor revisions > 500 are 
+    # reliable in that regard. For example minor revisions > 500 are
     # redundancy specific and < 500 are non-redundancy
     #
     # In most but not all cases, the major revision is the one people care
     # about and often times a controller revision discussed by only referring
     # the major revision. IE: "That controller is running revision 33", which
     # is why I've put effort into allowing the revision to be specified in that
-    #  way 
+    #  way
     #
-    # MicroLogix or motor controllers or stratix may to be different but let's 
+    # MicroLogix or motor controllers or stratix may to be different but let's
     # cross that bridge when we get there.
     # - AN
 
-    # Dictionary to hold major and minor revisions, will eventually be in the 
+    # Dictionary to hold major and minor revisions, will eventually be in the
     # form of { 'major': 00, 'minor': 00 }
-    revision_param = { }
-    
+    revision_param = {}
+
     ####################
     # Argument Parsing #
     ####################
     # Parse the revision information given to us. If it contains a period, that
     # implies it is in the major.minor form
-    if '.' in module.params['revision']:
+    if "." in module.params["revision"]:
 
         # Fail if more than a single decimal is provided
-        if module.params['revision'].count('.') > 1:
+        if module.params["revision"].count(".") > 1:
             module.fail_json(
-                msg="Controller revision not specified properly. Tried to parse revision `%s` in form Major.Minor, but more then one decimal was provided" % 
-                ( module.params['revision'] )
+                msg=(
+                    "Controller revision not specified properly. ",
+                    "Tried to parse revision `%s` in form Major.Minor,",
+                    "but more then one decimal was provided"
+                )
+                % (module.params["revision"])
             )
-        
-        # Determine the index of said period, and slice the revision string
-        dot_index = module.params['revision'].index('.')
-        major_slice = module.params['revision'][:dot_index]
-        minor_slice = module.params['revision'][dot_index + 1:]
 
-        # Attempt to convert major and minor slices. Ask for forgiveness if it 
+        # Determine the index of said period, and slice the revision string
+        dot_index = module.params["revision"].index(".")
+        major_slice = module.params["revision"][:dot_index]
+        minor_slice = module.params["revision"][dot_index + 1:]
+
+        # Attempt to convert major and minor slices. Ask for forgiveness if it
         # fails
         try:
-            revision_param['major'] = int(major_slice)
+            revision_param["major"] = int(major_slice)
         except ValueError:
-            # TODO How to properly fail with invalid parameters? 
+            # TODO How to properly fail with invalid parameters?
             module.fail_json(
-                msg="Controller revision not specified properly. Tried to parse revision `%s` in form Major.Minor, but could not parse major revision `%s` as an integer" % 
-                ( module.params['revision'], major_slice )
+                msg=(
+                    "Controller revision not specified properly. ",
+                    "Tried to parse revision `%s` in form Major.Minor, ",
+                    "but could not parse major revision `%s` as an integer"
+                )
+                % (module.params["revision"], major_slice)
             )
 
         try:
-            revision_param['minor'] = int(minor_slice)
+            revision_param["minor"] = int(minor_slice)
         except ValueError:
             module.fail_json(
-                msg="Controller revision not specified properly. Tried to parse revision `%s` in form Major.Minor, but could not parse minor revision `%s` as an integer" % 
-                ( module.params['revision'], minor_slice )
+                msg=(
+                    "Controller revision not specified properly. ",
+                    "Tried to parse revision `%s` in form Major.Minor, ",
+                    "but could not parse minor revision `%s` as an integer"
+                )
+                % (module.params["revision"], minor_slice)
             )
     else:
-        # If no period is found in the revision string, this implies that the 
+        # If no period is found in the revision string, this implies that the
         # string an integer representing the major revision
         try:
-            revision_param['major'] = int(module.params['revision'])
+            revision_param["major"] = int(module.params["revision"])
         except ValueError:
             module.fail_json(
-                msg="Controller revision not specified properly. Tried to parse revision `%s` in form `Major`, but could not parse major revision %s as an integer" % 
-                ( module.params['revision'], module.params['revision'] )
+                msg=(
+                    "Controller revision not specified properly. ",
+                    "Tried to parse revision `%s` in form `Major`, ",
+                    "but could not parse major revision %s as an integer"
+                )
+                % (module.params["revision"], module.params["revision"])
             )
-
 
     #####################
     # Revision Checking #
     #####################
 
     # check major revision
-    if plc_revision['major'] != revision_param['major']:
+    if plc_revision["major"] != revision_param["major"]:
         module.fail_json(
-            msg="Major revision %s not confirmed. Major revision %s found instead." % (
-                revision_param['major'], plc_revision['major']
+            msg=(
+                "Major revision %s not confirmed. ",
+                "Major revision %s found instead."
             )
+            % (revision_param["major"], plc_revision["major"])
         )
 
     # Check if minor revision was specified
-    if 'minor' in revision_param:
+    if "minor" in revision_param:
         # Check minor revision
-        if plc_revision['minor'] != revision_param['minor']:
+        if plc_revision["minor"] != revision_param["minor"]:
             module.fail_json(
-                msg="Major revision %s confirmed but minor revision %s not confirmed. Minor revision %s found instead." % (
-                    revision_param['major'], revision_param['minor'], plc_revision['minor']
+                msg=(
+                    "Major revision %s confirmed but minor revision %s not ",
+                    "confirmed. Minor revision %s found instead."
                 )
-        )
+                % (
+                    revision_param["major"],
+                    revision_param["minor"],
+                    plc_revision["minor"],
+                )
+            )
 
-
-    # Exit successfully. Check if for minor again to have a specific exit message
-    # for each case
+    # Exit successfully. Check if for minor again to have a specific exit
+    # message for each case
     if "minor" in revision_param:
         module.exit_json(
-            msg="Revision %s.%s confirmed." % (
-                revision_param['major'], revision_param['minor']
-            ),
-            changed=False
+            msg="Revision %s.%s confirmed."
+            % (revision_param["major"], revision_param["minor"]),
+            changed=False,
         )
     else:
         module.exit_json(
-            msg="Revision %s.%s matches major revision %s." % (
-                plc_revision['major'], plc_revision['minor'], revision_param['major'] ), changed=False
+            msg="Revision %s.%s matches major revision %s."
+            % (
+                plc_revision["major"],
+                plc_revision["minor"],
+                revision_param["major"]
+            ),
+            changed=False,
         )
+
 
 if __name__ == "__main__":
     main()
